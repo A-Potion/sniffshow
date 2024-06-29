d7f88ec4fd3062bf8e51ecf0096a459c559e4a22
Kacper <07Potion@gmail.com>
2024-06-29 22:05:14 +0200

    new views! and models!

--- dog/views.py
+++ dog/views.py
@@ -5,3 +5,14 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the dog index.")
+
+def detail(request, question_id):
+    response = "You're on the details page of the question %s."
+    return HttpResponse(response % question_id)
+
+def results(request, question_id):
+    response = "You're on the results page of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're on the voting page of the question %s." % question_id)
